f65094aec307c9e07a3ee78e7137c770ac2ed872
Stephen <stephen@umw.edu>
2015-12-23 12:52:15 -0800

    Implement Agent and RandomAgent.

--- R/Agent.R
+++ R/Agent.R
@@ -1,16 +1,69 @@
 
+library(methods)
+
+source("parameters.R")
+
+#############################################################################
 # Agent class. An agent is an individual who, if not already retired, will
 # decide whether to retire. This is the abstract superclass of all specific
 # subtypes with different retirement-decision algorithms.
-
-library(methods)
 
 Agent <- setRefClass("Agent",
     fields = list(
         age="numeric", 
         death.age="numeric", 
         state="character" # should be factor(c("working","retired","dead")).
+    ),
+    methods = list(
+        initialize=function(...) {
+            if (is.null(list(...)$age)) {
+                stop("Agent not initialized with age.")
+            }
+            death.age <<- sample(DEATH.AGE.RANGE[1]:DEATH.AGE.RANGE[2],1)
+            state <<- "working"
+        }
+    )
+    
+)
+
+Agent$methods(possibly.retire=function() {
+    stopifnot(state=="working")
+    if (.self$decide.whether.to.retire()) {
+        state <<- "retired"
+        return(TRUE)
+    } else {
+        return(FALSE)
+    }
+})
+
+Agent$methods(decide.whether.to.retire=function() {
+    stop("Abstract method decide.whether.to.retire() called.")
+})
+
+
+
+
+
+#############################################################################
+# RandomAgent class. Each time it is queried, a RandomAgent chooses to retire
+# with fixed probability.
+
+RandomAgent <- setRefClass("RandomAgent",
+    contains="Agent",
+    fields = list(
+        retirement.probability="numeric"
+    ),
+    methods = list(
+        initialize=function(retirement.probability=0, ...) { 
+            callSuper(...)
+            retirement.probability <<- retirement.probability
+        }
     )
 )
 
-stephen <- Agent$new(age=46,death.age=72,state="working")
+RandomAgent$methods(decide.whether.to.retire=function() {
+    return(runif(1) < retirement.probability)
+})
+
+
+stephen <- RandomAgent$new(retirement.probability=.3,age=46)
