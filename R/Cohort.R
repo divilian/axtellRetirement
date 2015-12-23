b3b51f73feca4407758805edc8d571bd647c4ae9
Stephen <stephen@umw.edu>
2015-12-23 13:56:17 -0800

    Add fill(), num.agents() methods.

--- R/Cohort.R
+++ R/Cohort.R
@@ -23,6 +23,15 @@ Cohort <- setRefClass("Cohort",
 
 Cohort$methods(advance.age=function() age <<- age + 1)
 
+Cohort$methods(fill=function(num.agents) {
+    members <<- vector("list", num.agents)
+    for (i in 1:num.agents) {
+        members[[i]] <<- Agent$new(age=age)
+    }
+})
+
+Cohort$methods(num.agents=function() length(members))
+
 
 
 
