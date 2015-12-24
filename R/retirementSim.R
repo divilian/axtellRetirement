da0a2a6d2e98352e9dd89865c19c95129de51143
Stephen <stephen@umw.edu>
2015-12-23 21:07:46 -0800

    Add main retirement() function and refactor.

--- R/retirementSim.R
+++ R/retirementSim.R
@@ -11,14 +11,26 @@ source("Cohort.R")
 # Cohorts
 
 # The all.cohorts global variable is an indexed list of Cohort objects.
-all.cohorts <- vector("list", max(SPAWN.AGE.RANGE))
-for (age in 1:max(SPAWN.AGE.RANGE)) {
-    all.cohorts[[age]] <- Cohort$new(age=age)
-}
-for (age in SPAWN.AGE.RANGE) {
-    all.cohorts[[age]]$fill(AGENTS.PER.COHORT)
+initialize.cohorts <- function() {
+    all.cohorts <<- vector("list", max(SPAWN.AGE.RANGE))
+    for (age in 1:max(SPAWN.AGE.RANGE)) {
+        all.cohorts[[age]] <- Cohort$new(age=age)
+    }
+    for (age in SPAWN.AGE.RANGE) {
+        all.cohorts[[age]]$fill(AGENTS.PER.COHORT)
+    }
 }
 
 advance.cohorts <- function() {
     all.cohorts <<- c(Cohort$new(age=1),all.cohorts[1:(max(SPAWN.AGE.RANGE)-1)])
+}
+
+
+
+##############################################################################
+# Main.
+retirement <- function() {
+
+    initialize.cohorts()   # (which also initializes agents.)
+
 }
